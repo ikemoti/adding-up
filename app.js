84f869e868e7a127b09eddab06d528866a3abe1d
“【ikemoti】” <ikepon413@icloud.com>
2021-03-19 18:48:14 +0900

    データの抜き出し

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ const rs = fs.createReadStream('./popu-pref.csv')
 const ls = readline.createInterface({ input: rs, output: {} })
 //node.jsにおけるストリーム　＝＝　非同期で情報を操る概念？？RX
 ls.on('line', lineString => {
-  console.log(lineString);
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
+  if (year === 2010 || year === 2015) {
+    console.log(year);
+    console.log(prefecture);
+    console.log(popu);
+  }
 });
