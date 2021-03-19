4ca7f3b51bd940b6a1ed29e9701f8567dceeee85
“【ikemoti】” <ikepon413@icloud.com>
2021-03-19 18:37:34 +0900

    CSVファイル読み込み

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+//importみたいなやつ
+const fs = require("fs")
+const readline = require("readline")
+
+const rs = fs.createReadStream('./popu-pref.csv')
+const ls = readline.createInterface({ input: rs, output: {} })
+//node.jsにおけるストリーム　＝＝　非同期で情報を操る概念？？RX
+ls.on('line', lineString => {
+  console.log(lineString);
+});
